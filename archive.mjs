e8f2ad294a41f695f87e9b13143649e8e81b3290
Xparham <xparham@gmail.com>
2021-02-08 15:06:45 -0500

    fixed typo

---
+++ archive.mjs
@@ -0,0 +1,29 @@
+export const person = {
+    name: {
+        fName: "Xavier",
+        lName: "Parham",
+        hub: "https://github.com/Xparham",
+        mail: "xparham@gmail.com"
+    },
+    location: {
+        city : "Boston",
+        state : "MA"
+    },
+    orgs: {
+        first: "MSIMBO Program",
+        second : "Urban league",
+    },
+
+    edu: {
+        high: "Boston Latin Academy",
+        uni: "Umass Lowell",
+        concen: "Animation"
+    },
+
+    community: {
+        heroes: "City Year",
+        camp: "C5 New England (formerly known as Camp Coca Cola Leadership Program)",
+        cares: "Boston Cares"
+    },
+};
+
