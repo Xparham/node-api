e20d76e719e5ba22b47219d31142ebc0c13ad6e8
Xparham <xparham@gmail.com>
2021-02-08 14:13:06 -0500

    created 'listen' for local host

--- server.mjs
+++ server.mjs
@@ -2,7 +2,15 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', () => {
+app.get('/welcome_friend', (req, res) => {
+    res.send('welcome friend')
 
-    
+})
+
+app.get('/', (req, res) => {
+    res.send('welcome home')
+})
+
+app.listen('3333', () => {
+    console.log('booting')
 })
