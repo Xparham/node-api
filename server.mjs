a933a1534b83cd287eb28a2dcc093ddae28f5b37
Xparham <xparham@gmail.com>
2021-02-08 17:56:17 -0500

    added GET's for first and last name

--- server.mjs
+++ server.mjs
@@ -1,24 +1,49 @@
 import express from 'express'
 import { person } from './archive.mjs'
 
+
 const PORT = 3333
 
 const app = express()
 
 app.get('/welcome', (req, res) => {
-    res.send('welcome!  ')
+    res.send(`Welcome viewer! This is my Resume's API!  Feel free to use command "npx xaviers-resume" to view my resume via terminal`)
 
 })
 
-app.get('/location', (req, res) =>{
+app.get('/person', (req, res) =>{
     res.send(person)
 })
 
-app.get('/id',(req, res) => {
-    res.send({
-        id: Math.random()
-    })
+app.get('/name', (req, res) =>{
+    res.send(`Xaiver Parham`)
 })
+
+app.get('/name/first', (req, res) =>{
+    res.send(`Xaiver`)
+})
+
+app.get('/name/last', (req, res) =>{
+    res.send(`Parham`)
+})
+
+app.get('/name', (req, res) =>{
+    res.send(`Xaiver Parham`)
+})
+
+app.get('/name', (req, res) =>{
+    res.send(`Xaiver Parham`)
+})
+
+app.get('/name', (req, res) =>{
+    res.send(`Xaiver Parham`)
+})
+
+// app.get('/id',(req, res) => {
+//     res.send({
+//         id: Math.random()
+//     })
+// })
 
 
 app.listen(PORT, () => {
