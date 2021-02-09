c871d4cb5464d9d02bf07f56d7dc531b89ec2f32
Xparham <xparham@gmail.com>
2021-02-09 16:55:09 -0500

    refactored API correctly

--- archive.mjs
+++ archive.mjs
@@ -1,45 +1,107 @@
 export const person = {
-    name: {
-        fName: "Xavier",
-        lName: "Parham",
-        hub: "https://github.com/Xparham",
-        vercel: ""
-        mail: "xparham@gmail.com"
-    },
-    location: {
-        city : "Boston",
-        state : "MA"
-    },
-    orgs: {
-        first: "MSIMBO Program",
-        second : "Urban league",
+    "basics": {
+        "mName": "Xavier Parham",
+        "label": "Web Developer/Programmer",
+        "mail": "xparham@gmail.com",
+        "location": {
+            "city" : "Boston",
+            "state" : "MA",
+            "zipCode": "02125",
+            "countryCode": "US",
+            "region": "New England"
+        },
+    
+        "profiles": [{
+            "github": "GitHub",
+            "gitUsername": "Username:Xparham",
+            "gitUrl": "URL:https://github.com/Xparham",
+            "vercel": "Vercel",
+            "vUsername": "Username:Xparham",
+            "vUrl":"URL:https://vercel.com/xparham",
+            "npm": "npm",
+            "nUsername": "Username:xparham",
+            "nUrl": "URL:https://www.npmjs.com/~xparham"
+
+        }]
     },
 
-    languages : {
-        // frontend
-        html:"HTML", 
-        css: "CSS(Tailwind CSS)", 
-        java: "JavaScript",
-        // backend
-        javaE: "JavaScript ESL6", 
-        node:"Node.js",
-        express: "Express frameworks", 
-        react:"React frameworks",
-        
-        
-        other: "Github, Command Line, "
+    "urbanLeague": {
+        "company": "MSIMBO Program",
+        "website": "https://msimbo.herokuapp.com/",
+        "startDate":"2020-20-10",
+        "endDate": "2021-8-3",
+        "summary": "MSIMBO is a 20 week coding program for urbanites interested in becoming programmers.  It is a division of the Urban League of Eastern Massachusetts that trains indivduals to become full-stack developers.  Trainees are conditioned in web development,  business communication, and leadership."
+    },
+
+    "skills": {
+        "traits":"Web Development/Animator",
+        "level": "Intermediate",
+        "keywords": [
+            "HTML",
+            "CSS(Tailwind CSS)", 
+            "JavaScript",
+            "JavaScript ESL6+", 
+            "Node.js",
+            "Express frameworks", 
+            "React frameworks",
+            "comprehension of command-line with Git",
+            "Adobe XD",
+            "Adobe Animate",
+            "Adobe After-Effects",
+            "Adobe Photoshop",
+            "MAYA 3D modeling",
+          
+        ]
+
     }, 
 
-    edu: {
-        high: "Boston Latin Academy",
-        uni: "Umass Lowell",
-        concen: "Animation"
+
+    "edu": {
+        "institution":"Boston Latin Academy",
+        "studyType": "High School Diploma",
+        "startDate": "2008-11-09",
+        "endDate": "2012-10-06",
+        "gpa": "3.3",
+
+        "institution":"UMASS Lowell",
+        "area":"Fine Arts/Concentration Animation",
+        "studyType": "Bachelor",
+        "startDate":"2012-09-11",
+        "endDate":"currently enrolled",
+        "gpa":"3.4"
     },
 
-    community: {
-        heroes: "City Year",
-        camp: "C5 New England (formerly known as Camp Coca Cola Leadership Program)",
-        cares: "Boston Cares"
+    
+
+    "volunteer": [{
+        "organization": "City Year",
+        "position": "volunteer",
+        "website": "https://www.cityyear.org/boston/",
+        "startDate":"2005-08-10",
+        "endDate":"2007-12-01",
+        "summary":"City year is a NPO that pursues individuals to make an impact within their community through education and community service",
+        
+        
+        "organization": "C5 New England (formerly known as Camp Coca Cola Leadership Program)",
+        "position":"Alumnus",
+        "website":"https://crossroadsma.org/programs/c5-new-england-program-overview/",
+        "startDate":"2007-07-06",
+        "endDate":"2012-11-06",
+        "summary":"C5 is a five year leadership program that instills values of hard-work, integrity, and leadership through academic and civic facets.",
+        
+        "organization":"Boston Cares",
+        "position":"volunteer",
+        "website":"https://www.bostoncares.org/?layoutViewMode=tablet",
+        "startDate":"2010-01-26",
+        "endDate":"2012-01-30",
+        "summary":"Boston Cares is the largest Volunteer organization in New England that allows members to serve their community through affiliates of the program."
+
+    }],
+
+    "languages": {
+        "language" : "English",
+        "fluency" : "Native Speaker"
     },
 };
+
 
