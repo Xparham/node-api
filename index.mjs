6b7a5631ec2ae3186eeeadefbe1a9434993ebde9
Xparham <xparham@gmail.com>
2021-02-09 18:03:51 -0500

    testing data extraction

--- index.mjs
+++ index.mjs
@@ -2,21 +2,20 @@
 import { person } from './archive.mjs'
 
 
-const {location:{city, state}} = person;
-const {name:{fName, lName, mail, hub}} = person ;
-const {orgs:{first, second}} = person;
-const {edu:{high, uni, concen}} = person;
-const {community:{heroes, camp, cares}} = person;
+// const {basics:{nName, label, mail, location}} = person;
 
-console.log( `${fName} ${lName}`)
-console.log(`-------------------`)
-console.log(`${city}, ${state}`)
-console.log(`---------`)
-console.log(`I am currently in the ${first} with the ${second} steadfastly improving my knowlegde in front-end and back-end coding.`)
-console.log(`---------`)
-console.log(`My education history includes a HSD at ${high}, and working towards a BFA at ${uni} with a concentration in ${concen}`)
-console.log(`---------`)
-console.log(`I have community service experience with ${heroes} from 2005-2007, ${camp}, from 2007-2012, and ${cares} from 2010-2012.`)
-console.log(`-------------------`)
-console.log(`E-mail:${mail}`)
-console.log(`Github:${hub}`)
+
+// console.log( `${fName} ${lName}`)
+// console.log(`-------------------`)
+// console.log(`${city}, ${state}`)
+// console.log(`---------`)
+// console.log(`I am currently in the ${first} with the ${second} steadfastly improving my knowlegde in front-end and back-end coding.`)
+// console.log(`---------`)
+// console.log(`My education history includes a HSD at ${high}, and working towards a BFA at ${uni} with a concentration in ${concen}`)
+// console.log(`---------`)
+// console.log(`I have community service experience with ${heroes} from 2005-2007, ${camp}, from 2007-2012, and ${cares} from 2010-2012.`)
+// console.log(`-------------------`)
+// console.log(`E-mail:${mail}`)
+// console.log(`Github:${hub}`)
+
+console.log(person.basics.location.state)
