5b19257a6eae41d111372d6efd141ff804efacbb
Xparham <xparham@gmail.com>
2021-02-10 11:41:19 -0500

    test

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express ();
+
+const PORT = process.env.PORT || 3333;
+
+app.get ('/', (req, res) => {
+    res.send(`welcome`)
+});
+
+app.listen(PORT, () => {
+    console.log(`running on ${PORT}`)
+});
