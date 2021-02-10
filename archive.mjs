ee4a8979d6c9d12fc819404d88eb24d54269ea44
Xparham <xparham@gmail.com>
2021-02-09 19:08:20 -0500

    reorganized profiles section

--- archive.mjs
+++ archive.mjs
@@ -1,6 +1,6 @@
 export const person = {
     "basics": {
-        "mName": "Xavier Parham",
+        "id": "Xavier Parham",
         "label": "Web Developer/Programmer",
         "mail": "xparham@gmail.com",
         "location": {
@@ -11,18 +11,26 @@ export const person = {
             "region": "New England"
         },
     
-        "profiles": [{
-            "github": "GitHub",
-            "gitUsername": "Username:Xparham",
-            "gitUrl": "URL:https://github.com/Xparham",
-            "vercel": "Vercel",
-            "vUsername": "Username:Xparham",
-            "vUrl":"URL:https://vercel.com/xparham",
-            "npm": "npm",
-            "nUsername": "Username:xparham",
-            "nUrl": "URL:https://www.npmjs.com/~xparham"
+        "profiles": {
+            "gitHub": {
+                "site": "GitHub",
+                "username": "Username:Xparham",
+                "url": "URL:https://github.com/Xparham"
+            },
 
-        }]
+            "vercel":{
+                "site": "Vercel",
+                "username": "Username:Xparham",
+                "url":"URL:https://vercel.com/xparham"
+            },
+            
+            "npm": { 
+                "site": "NPM",
+                "username": "Username:xparham",
+                "url": "URL:https://www.npmjs.com/~xparham"
+            },
+
+        },
     },
 
     "urbanLeague": {
@@ -51,52 +59,61 @@ export const person = {
             "Adobe Photoshop",
             "MAYA 3D modeling",
           
-        ]
+        ],
 
     }, 
 
 
     "edu": {
-        "institution":"Boston Latin Academy",
-        "studyType": "High School Diploma",
-        "startDate": "2008-11-09",
-        "endDate": "2012-10-06",
-        "gpa": "3.3",
+        "highSchool":{
+            "institution":"Boston Latin Academy",
+            "studyType": "High School Diploma",
+            "startDate": "2008-11-09",
+            "endDate": "2012-10-06",
+            "gpa": "3.3",
+        },
 
-        "institution":"UMASS Lowell",
-        "area":"Fine Arts/Concentration Animation",
-        "studyType": "Bachelor",
-        "startDate":"2012-09-11",
-        "endDate":"currently enrolled",
-        "gpa":"3.4"
+        "college":{
+            "institution":"UMASS Lowell",
+            "area":"Fine Arts/Concentration Animation",
+            "studyType": "Bachelor",
+            "startDate":"2012-09-11",
+            "endDate":"currently enrolled",
+            "gpa":"3.4"
+        },
     },
 
     
 
-    "volunteer": [{
-        "organization": "City Year",
-        "position": "volunteer",
-        "website": "https://www.cityyear.org/boston/",
-        "startDate":"2005-08-10",
-        "endDate":"2007-12-01",
-        "summary":"City year is a NPO that pursues individuals to make an impact within their community through education and community service",
-        
-        
-        "organization": "C5 New England (formerly known as Camp Coca Cola Leadership Program)",
-        "position":"Alumnus",
-        "website":"https://crossroadsma.org/programs/c5-new-england-program-overview/",
-        "startDate":"2007-07-06",
-        "endDate":"2012-11-06",
-        "summary":"C5 is a five year leadership program that instills values of hard-work, integrity, and leadership through academic and civic facets.",
-        
-        "organization":"Boston Cares",
-        "position":"volunteer",
-        "website":"https://www.bostoncares.org/?layoutViewMode=tablet",
-        "startDate":"2010-01-26",
-        "endDate":"2012-01-30",
-        "summary":"Boston Cares is the largest Volunteer organization in New England that allows members to serve their community through affiliates of the program."
+    "volunteer": {
+        "cityYear":{
+            "organization": "City Year",
+            "position": "volunteer",
+            "website": "https://www.cityyear.org/boston/",
+            "startDate":"2005-08-10",
+            "endDate":"2007-12-01",
+            "summary":"City year is a NPO that pursues individuals to make an impact within their community through education and community service"
+        },
 
-    }],
+        "c5": {
+            "organization": "C5 New England (formerly known as Camp Coca Cola Leadership Program)",
+            "position":"Alumnus",
+            "website":"https://crossroadsma.org/programs/c5-new-england-program-overview/",
+            "startDate":"2007-07-06",
+            "endDate":"2012-11-06",
+            "summary":"C5 is a five year leadership program that instills values of hard-work, integrity, and leadership through academic and civic facets."
+        },
+
+        "bostonCares": {
+            "organization":"Boston Cares",
+            "position":"volunteer",
+            "website":"https://www.bostoncares.org/?layoutViewMode=tablet",
+            "startDate":"2010-01-26",
+            "endDate":"2012-01-30",
+            "summary":"Boston Cares is the largest Volunteer organization in New England that allows members to serve their community through affiliates of the program."
+        },
+
+    },
 
     "languages": {
         "language" : "English",
